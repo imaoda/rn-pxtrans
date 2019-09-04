d589bf76fbc1a935ce77725c176df4fb23d0ea0b
王永峰 <wangyongfeng03@meituan.com>
2019-09-04 14:43:40 +0800

    修复ts错误

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,23 @@
-import { StyleSheet as NStyle } from '@types/react-native'
+import {
+  StyleSheet as NStyle,
+  ViewStyle,
+  TextStyle,
+  ImageStyle
+} from "@types/react-native";
 /**
  * 对单个尺寸进行转换
  */
-export function pxTrans(width: number): number
+export function pxTrans(width: number): number;
 
 /**
  * 在进行 styleTrans 或在 StyleCreate 时候，如果某个尺寸想使用原始 px，则用此函数包裹
  */
-export function noTrans(width: number): number
+export function noTrans(width: number): number;
 
 /**
  * 非 StyleSheet.create 创建的样式，通过该函数可执行全量的 px 转换
  */
-export function styleTrans<T extends NStyle.NamedStyles<T>>(styles: T): T
+export function styleTrans(styles: ViewStyle | TextStyle | ImageStyle): any;
 
 /**
  * 初始化函数，劫持 StyleSheet.create
@@ -23,4 +28,6 @@ export function styleTrans<T extends NStyle.NamedStyles<T>>(styles: T): T
 // export function initTrans(StyleSheet: any, Dimensions: any, size: number = 750): void
 
 declare type RegisteredStyle<T> = number & { __registeredStyleBrand: T };
-export function create<T extends NStyle.NamedStyles<T>>(styles: T): { [P in keyof T]: RegisteredStyle<T[P]> };
+export function create<T extends NStyle.NamedStyles<T>>(
+  styles: T
+): { [P in keyof T]: RegisteredStyle<T[P]> };
