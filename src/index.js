0015a5fc0ea2a2ce23259fb661c855da15d6da78
王永峰 <wangyongfeng03@meituan.com>
2019-09-04 19:08:45 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,44 +1,57 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet, Dimensions } from "react-native";
 
-const deviceWidthDp = Dimensions.get('window').width;
-const baseWidth = 750
+const deviceWidthDp = Dimensions.get("window").width;
+const baseWidth = 750;
 
-const reg1 = /(width|height|radius|size|top|bottom|left|right|flexBasis|margin|padding)/i
-const reg2 = /shadowOffset$/i
+const reg1 = /(width|height|radius|size|top|bottom|left|right|flexBasis|margin|padding)/i;
+const reg2 = /shadowOffset$/i;
 
 export function pxTrans(width) {
-  return deviceWidthDp * width / baseWidth;
+  return (deviceWidthDp * width) / baseWidth;
 }
 
 export function noTrans(width) {
-  return baseWidth * width / deviceWidthDp
+  return (baseWidth * width) / deviceWidthDp;
 }
 
-export function styleTrans(obj) {
-  if (!obj) return {}
-  const newObj = JSON.parse(JSON.stringify(obj))
+export function styleTrans(style) {
+  style = JSON.parse(JSON.stringify(style));
+  Object.keys(style).forEach(cssName => {
+    const cssValue = style[cssName];
+    if (cssName === "tranform" && cssValue instanceof Array) {
+      cssValue.forEach(item => {
+        if (typeof item.translateX === "number")
+          item.translateX = pxTrans(item.translateX);
+        if (typeof item.translateY === "number")
+          item.translateY = pxTrans(item.translateY);
+      });
+    }
+    if (
+      reg1.test(cssName) &&
+      typeof cssValue === "number" &&
+      !isNaN(cssValue)
+    ) {
+      style[cssName] = pxTrans(cssValue);
+    }
+    if (reg2.test(cssName)) {
+      if (typeof style[cssName].height === "number")
+        style[cssName].height = pxTrans(style[cssName].height);
+      if (typeof style[cssName].width === "number")
+        style[cssName].width = pxTrans(style[cssName].width);
+    }
+  });
+  return style;
+}
+
+function styleTransAll(obj) {
+  if (!obj) return {};
+  const newObj = { ...obj };
   Object.keys(newObj).forEach(k => {
-    const style = newObj[k]
-    Object.keys(style).forEach(cssName => {
-      const cssValue = style[cssName]
-      if (cssName === 'tranform' && cssValue instanceof Array) {
-        cssValue.forEach(item => {
-          if (typeof item.translateX === 'number') item.translateX = pxTrans(item.translateX)
-          if (typeof item.translateY === 'number') item.translateY = pxTrans(item.translateY)
-        });
-      }
-      if (reg1.test(cssName) && typeof cssValue === 'number' && !isNaN(cssValue)) {
-        style[cssName] = pxTrans(cssValue)
-      }
-      if (reg2.test(cssName)) {
-        if (typeof style[cssName].height === 'number') style[cssName].height = pxTrans(style[cssName].height)
-        if (typeof style[cssName].width === 'number') style[cssName].width = pxTrans(style[cssName].width)
-      }
-    })
-  })
-  return newObj
+    newObj[k] = styleTrans(newObj[k]);
+  });
+  return newObj;
 }
 
 export function create(obj) {
-  return StyleSheet.create(styleTrans(obj))
+  return StyleSheet.create(styleTransAll(obj));
 }
